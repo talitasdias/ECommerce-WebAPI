363e82c545dd36946027b09e094d46433dc5ca0a
Talita Dias <talita.dias@itriad.org.br>
2024-12-04 10:23:22 -0400

    initial commit

---
+++ Ecommerce/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.DataBase;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ECommerceDbContext _context;
+
+        public CategoryController(ECommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(_context.Categories.ToList());
+        }
+    }
+}
---
+++ Ecommerce/DataBase/ECommerceDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Ecommerce.DataBase
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}
+
+public class Category
+{
+    [Key]
+    [Column(TypeName = "text")]
+    public string ID { get; set; }
+
+    [Column(TypeName = "text")]
+    public string Title { get; set; }
+
+    [Column(TypeName = "text")]
+    public string Description { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime CreatedAt { get; set; }
+}
