b205484d9b57b9e03c25f30d4ecac1b3d6027a5e
Talita Dias <talita.dias@itriad.org.br>
2024-12-04 13:56:56 -0400

    feat: implement UpdateCategory endpoint

--- Ecommerce/Controllers/CategoryController.cs
+++ Ecommerce/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Ecommerce.DataBase;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Controllers
 {
@@ -37,6 +38,24 @@ namespace Ecommerce.Controllers
             {
                 var category = _context.Categories.FirstOrDefault(category => category.ID == id);
                 return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("UpdateCategory{id}")]
+        public async Task<IActionResult> Update([FromBody] Category category, string id)
+        {
+            try
+            {
+                Category categoryData = _context.Categories.FirstOrDefault(category => category.ID == id);
+                categoryData.Title = category.Title;
+                categoryData.Description = category.Description;
+                _context.Categories.Update(categoryData);
+                await _context.SaveChangesAsync();
+                return Ok(categoryData);
             }
             catch (Exception ex)
             {
