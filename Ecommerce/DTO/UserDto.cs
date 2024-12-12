4a7c1d3d2db98393ab8081abdb2fb99bda7203fe
Talita Dias <talita.dias@itriad.org.br>
2024-12-12 15:29:55 -0400

    feat: add UserDto

---
+++ Ecommerce/DTO/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.DTO
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
