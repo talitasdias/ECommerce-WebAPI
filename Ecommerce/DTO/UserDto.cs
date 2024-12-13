17769e55814dd4bd4253cf63549092e410c5bb90
Talita Dias <talita.dias@itriad.org.br>
2024-12-13 14:40:08 -0400

    insert required in the dtos

--- Ecommerce/DTO/UserDto.cs
+++ Ecommerce/DTO/UserDto.cs
@@ -2,9 +2,9 @@
 {
     public class UserDto
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string ConfirmPassword { get; set; }
     }
 }
