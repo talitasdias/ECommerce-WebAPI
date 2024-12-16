e6fefe6a7fcf1148160f3e426c72a5b2068dd686
Talita Dias <talita.dias@itriad.org.br>
2024-12-16 11:24:52 -0400

    feat: create product dto

---
+++ Ecommerce/DTO/ProductDTO.cs
@@ -0,0 +1,12 @@
+﻿using Ecommerce.Entities;
+
+namespace Ecommerce.DTO
+{
+    public class ProductDTO
+    {
+        public required string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
