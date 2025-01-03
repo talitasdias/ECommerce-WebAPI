482a1dfbf200a57a5a5390f97177b253527e23bc
Talita Dias <talita.dias@itriad.org.br>
2025-01-03 11:23:05 -0400

    feat:create DTO to get product

---
+++ Ecommerce/DTO/GetProductDTO.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Entities;
+
+namespace Ecommerce.DTO
+{
+    public class GetProductDTO
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public required CategoryEntity Category { get; set; }
+    }
+}
