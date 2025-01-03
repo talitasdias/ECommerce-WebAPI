bb942c666453568a039fa556f752a191e891d68e
Talita Dias <talita.dias@itriad.org.br>
2025-01-03 09:52:01 -0400

    feat:add DTOs to create product

---
+++ Ecommerce/DTO/CreateProductOutputDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Ecommerce.DTO
+{
+    public class CreateProductOutputDTO
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
